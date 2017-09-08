d8e1f57e4957569da03367963dbab87a6990dc94
vahedi25411 <m.vahedi@gmail.com>
2017-09-07 21:35:17 -0700

    Restore changes in master

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -9,12 +9,8 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
-        with open(file_path) as f:
-            for line in f:
-                self.numbers.append([int(x) for x in line.split()])
 
     def get_mean(self, line_number):
-        return get_avg(self.numbers[line_number])
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
@@ -22,7 +18,6 @@ class SimpleFile(object):
         pass
 
     def get_max(self, line_number):
-        return max(self.numbers[line_number])
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
@@ -30,7 +25,6 @@ class SimpleFile(object):
         pass
 
     def get_min(self, line_number):
-        return min(self.numbers[line_number])
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
@@ -38,7 +32,6 @@ class SimpleFile(object):
         pass
 
     def get_sum(self, line_number):
-        return get_sum(self.numbers[line_number])
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
